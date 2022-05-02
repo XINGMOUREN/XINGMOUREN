ba5a368de97d179917151e88aeff3c7d1933d451
Happy King <2638526782@qq.com>
2022-05-02 09:57:34 +0800

    Update background.js

--- js/background.js
+++ js/background.js
@@ -3,6 +3,11 @@ var backimg =[
     "url(https://pic.imgdb.cn/item/625d7347239250f7c5cb7976.jpg)",
     "url(https://pic.imgdb.cn/item/625d7a03239250f7c5dd3061.jpg)",
     "url(https://pic.imgdb.cn/item/625d733b239250f7c5cb558b.png)",
+    "url(https://pic.imgdb.cn/item/626f392f239250f7c523f794.png)",
+    "url(https://pic.imgdb.cn/item/626f39c0239250f7c5251d52.jpg)",
+    "url(https://www.z4a.net/images/2022/05/02/79776133_p0.png)",
+    "url(https://pic.imgdb.cn/item/626f3a0c239250f7c525adf0.png)",
+    "url(https://pic.imgdb.cn/item/626f3a2a239250f7c525fbea.jpg)",
     "url(https://pic.imgdb.cn/item/625d7324239250f7c5cb0b61.png)"
   ];
   //获取背景图片总数，生成随机数
@@ -14,6 +19,11 @@ var backimg =[
     "url(https://pic.imgdb.cn/item/625d7347239250f7c5cb7976.jpg)",
     "url(https://pic.imgdb.cn/item/625d7a03239250f7c5dd3061.jpg)",
     "url(https://pic.imgdb.cn/item/625d733b239250f7c5cb558b.png)",
+    "url(https://pic.imgdb.cn/item/626f392f239250f7c523f794.png)",
+    "url(https://pic.imgdb.cn/item/626f39c0239250f7c5251d52.jpg)",
+    "url(https://www.z4a.net/images/2022/05/02/79776133_p0.png)",
+    "url(https://pic.imgdb.cn/item/626f3a0c239250f7c525adf0.png)",
+    "url(https://pic.imgdb.cn/item/626f3a2a239250f7c525fbea.jpg)",
     "url(https://pic.imgdb.cn/item/625d7324239250f7c5cb0b61.png)"
   ];
   //获取banner图片总数，生成随机数
