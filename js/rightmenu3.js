d7657f1c23f9996726d4d4a60abbd73f899e6c1d
Happy King <2638526782@qq.com>
2022-05-02 10:10:58 +0800

    Update rightmenu3.js

--- js/rightmenu3.js
+++ js/rightmenu3.js
@@ -72,7 +72,42 @@ kk.copySelect = function(){
     alert("复制成功！祝你今天开心哦");
     //这里可以写点东西提示一下 已复制
 }
+kk.switchDarkMode = function(){
+    const nowMode = document.documentElement.getAttribute('data-theme') === 'dark' ? 'dark' : 'light'
+    if (nowMode === 'light') {
+        activateDarkMode()
+        saveToLocal.set('theme', 'dark', 2)
+        GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.day_to_night)
+    } else {
+        activateLightMode()
+        saveToLocal.set('theme', 'light', 2)
+        GLOBAL_CONFIG.Snackbar !== undefined && btf.snackbarShow(GLOBAL_CONFIG.Snackbar.night_to_day)
+    }
+    // handle some cases
+    typeof utterancesTheme === 'function' && utterancesTheme()
+    typeof FB === 'object' && window.loadFBComment()
+    window.DISQUS && document.getElementById('disqus_thread').children.length && setTimeout(() => window.disqusReset(), 200)
+};
+kk.switchReadMode = function(){
+    const $body = document.body
+    $body.classList.add('read-mode')
+    const newEle = document.createElement('button')
+    newEle.type = 'button'
+    newEle.className = 'fas fa-sign-out-alt exit-readmode'
+    $body.appendChild(newEle)
 
+    function clickFn () {
+        $body.classList.remove('read-mode')
+        newEle.remove()
+        newEle.removeEventListener('click', clickFn)
+    }
+
+    newEle.addEventListener('click', clickFn)
+}
+
+
+$('#menu-darkmode').on('click',kk.switchDarkMode);
+$('#menu-readmode').on('click',kk.switchReadMode);
 kk.switchTheme=function(load=false){
     //空字符串表示butterfly原版主题（即不加载css）
     //FallGuys.css是我自己的魔改主题，需替换
